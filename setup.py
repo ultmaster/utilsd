a18d8b796c65a89008b0ea9c94361fcca8bde0e1
Yuge Zhang <Yuge.Zhang@microsoft.com>
2021-03-19 14:26:39 +0800

    Fix python version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     extras_require={
         'full': ['mmcv>=1.2.7']
     }
