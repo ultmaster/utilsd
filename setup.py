2107f056f904e764c3cfc2cdb055dc87c5c202d0
Yuge Zhang <Yuge.Zhang@microsoft.com>
2021-04-25 14:06:03 +0800

    Refine setup.py to make versioning automatic

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+import os
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
     name='utilsd',
-    version='0.0.4',
+    version=get_version('utilsd/__init__.py'),
     author='Yuge Zhang',
     author_email='scottyugochang@gmail.com',
     description='Common utils for deep learning.',
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/ultmaster/utilsd',
     packages=setuptools.find_packages(),
@@ -20,10 +34,10 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     install_requires=[
-        'addict',
-        'numpy',
-        'pyyaml',
-        'yapf',
+        'addict>=2.4.0',
+        'numpy>=1.18.0',
+        'pyyaml>=5.4.1',
+        'yapf>=0.31.0',
     ],
     extras_require={
         'full': ['torch>=1.7.1'],
--- utilsd/__init__.py
+++ utilsd/__init__.py
@@ -2,5 +2,4 @@ from .logging import print_log, setup_logger
 from .misc import (get_checkpoint_dir, get_output_dir, get_runtime_config, get_tb_log_dir, is_debugging,
                    setup_distributed_training, setup_experiment)
 
-# TODO: make this automatic
-__version__ = 'v0.0.4'
+__version__ = '0.0.4'
