843a214ba791d4d0bef8f452e1e71a89abea098e
Yuge Zhang <Yuge.Zhang@microsoft.com>
2021-06-07 16:30:13 +0800

    Add more shortcuts

--- utilsd/__init__.py
+++ utilsd/__init__.py
@@ -1,5 +1,7 @@
-from .logging import print_log, setup_logger
+from .config import *
 from .experiment import (get_checkpoint_dir, get_output_dir, get_runtime_config, get_tb_log_dir, is_debugging,
                          setup_distributed_training, setup_experiment, print_config, use_cuda)
+from .fileio import load, dump
+from .logging import print_log, setup_logger
 
 __version__ = '0.0.8'
